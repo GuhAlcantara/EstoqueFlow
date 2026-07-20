50e391e3f4fb376679b19ab14fcc68babc93b319
BIROSCA <BIROSCA@biroscahamb>
2026-07-20 17:02:16 -0300

    Primeiro Commit

---
+++ EstoqueFlow/src/entities/Insumos.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Insumos {
+	private String nome;
+	private String marca;
+	private int quantidade;
+	private double peso;
+	{
+
+	}
+
+	public Insumos() {
+	}
+
+	
+	
+}
