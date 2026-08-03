54d6412598510711e5a44d2fe9e2135c23943381
BIROSCA <gustavoamv@gmail.com>
2026-08-02 23:47:52 -0300

    Alteraçoes de classes para abstratas

--- EstoqueFlow/src/entities/Ingrediente.java
+++ EstoqueFlow/src/entities/Ingrediente.java
@@ -2,88 +2,20 @@ package entities;
 
 import java.util.Date;
 
-public class Ingrediente {
-	private static int ingredienteID;
-	private String nome, marca, lote;
-	private int quantidade;
-	private Date validade;
-	private double peso;
-	{
+public class Ingrediente extends Produto {
 
+	private double ingredienteQuantidade;
+
+	public Ingrediente(String nome, double ingredienteQuantidade, Date dataValidade ) {
+		super(nome, dataValidade);
+		this.ingredienteQuantidade  = ingredienteQuantidade;
 	}
 
-	public Ingrediente() {
-
+	@Override
+	public String toString() {
+		return "Produto Cadastrado: \n"
+				+"Nome: " + nome
+				+ "Quantidade: " + ingredienteQuantidade
+				+"Data de Validade: " + dataValidade;
 	}
-
-	public Ingrediente(String nome, String marca, String lote, int quantidade, Date validade, double peso) {
-		this.nome = nome;
-		this.marca = marca;
-		this.lote = lote;
-		this.quantidade = quantidade;
-		this.validade = validade;
-		this.peso = peso;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		if (nome != null) {
-			this.nome = nome;
-		}
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		if (marca != null) {
-			this.marca = marca;
-		}
-	}
-
-	public String getLote() {
-		return lote;
-	}
-
-	public void setLote(String lote) {
-		if (marca != null) {
-			this.lote = lote;
-		}
-	}
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		if (quantidade >= 0) {
-			this.quantidade = quantidade;
-		}
-
-	}
-
-	public Date getValidade() {
-		return validade;
-	}
-
-	public void setValidade(Date validade) {
-		if (validade != null) {
-			this.validade = validade;
-		}
-	}
-
-	public double getPeso() {
-		return peso;
-	}
-
-	public void setPeso(double peso) {
-		if (peso != 0) {
-			this.peso = peso;
-		}
-	}
-
 }
---
+++ EstoqueFlow/src/entities/Produto.java
@@ -0,0 +1,101 @@
+package entities;
+
+import java.util.Date;
+
+public class Produto {
+    protected String nome, lote, marca;
+    protected double quantidade, pesoProduto;
+    protected Date dataValidade;
+
+
+    public Produto(String nome,Date dataValidade) {
+        this.nome = nome;
+        this.dataValidade = dataValidade;
+    }
+
+            // CONTRUTOR PARA PRODUTOS EM ***QUATIDADE****
+    public Produto(String nome, String lote, String marca, double quantidade, Date data) {
+        this.nome = nome;
+        this.lote = lote;
+        this.marca = marca;
+        this.quantidade = quantidade;
+        this.dataValidade = data;
+    }
+
+            // CONSTRUTOR PARA PRODUTOS EM  ***PESO***
+    public Produto(String nome, String lote, String marca, double quantidade, double pesoProduto, Date data) {
+        this.nome = nome;
+        this.lote = lote;
+        this.marca = marca;
+        this.pesoProduto = pesoProduto;
+        this.dataValidade = data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null) {
+            this.nome = nome;
+        }
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        if (marca != null) {
+            this.lote = lote;
+        }
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        if (marca != null) {
+            this.marca = marca;
+        }
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        if (quantidade >= 0) {
+            this.quantidade = quantidade;
+        }
+
+    }
+
+    public double getPesoProduto() {
+        return pesoProduto;
+    }
+
+    public void setPesoProduto(double pesoProduto) {
+        if (pesoProduto != 0) {
+            this.pesoProduto = pesoProduto;
+        }
+    }
+
+    public Date getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setValidade(Date dataValidade) {
+        if (dataValidade != null) {
+            this.dataValidade = dataValidade;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Produto Cadastrado: \n"
+                +"Nome: " + nome
+                +"Data de Validade: " + dataValidade;
+    }
+}
